1c38f29c37306482d02f9d486a8ed33fa38e6c8d
ioncocieru <ionut.cocieru112@gmail.com>
2025-12-31 21:23:36 +0200

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const messages = {
   ru: {
-    title: "🚧 Сайт находится в разработке 🚧",
+    title: "Данный сайт припаркован в гараже у Cocieru Ion",
     text: "Совсем скоро здесь появится что-то интересное!",
     byline: "Сайт разрабатывается Cocieru Ion.",
     contact: "Связаться в Instagram"
   },
   en: {
-    title: "🚧 Website under construction 🚧",
+    title: "This website is parked in Cocieru Ion’s garage.",
     text: "Something exciting is coming very soon!",
     byline: "Site developed by Cocieru Ion.",
     contact: "Contact via Instagram"
   },
   ro: {
-    title: "🚧 Site în construcție 🚧",
+    title: "Acest site este parcat în garajul lui Cocieru Ion.",
     text: "Foarte curând aici va apărea ceva interesant!",
     byline: "Site dezvoltat de Cocieru Ion.",
     contact: "Contactează pe Instagram"
@@ -28,3 +28,4 @@ function setLang(lang) {
 
 // по умолчанию RU (можно выбрать другую)
 setLang("ru");
+
